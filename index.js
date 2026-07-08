fe0ca41f854f20b14f5937b8d1fe310095be9d1a
Artem-WebDeveloper <ave_art@icloud.com>
2026-07-08 14:35:13 +0300

    feat: change routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -43,6 +44,14 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'u
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
+dataObj.forEach(el => {
+  el.slug = slugify(el.productName, { lower: true });
+});
+// console.log(dataObj);
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+
+// console.log(slugs);
+
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
   console.log(pathname);
@@ -55,9 +64,14 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     // Product Page
-  } else if (pathname === '/product') {
+  } else if (pathname.startsWith('/product')) {
     res.writeHead(200, { 'Content-type': 'text/html' });
-    const product = dataObj[query.id];
+    console.log(query);
+    console.log(pathname);
+
+    // const product = dataObj[query.id];
+    const product = dataObj.find(el => el.slug === pathname.split('/')[2]);
+    console.log(product);
     const output = replaceTemplate(tempProduct, product);
 
     res.end(output);
